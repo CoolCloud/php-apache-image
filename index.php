4b34898cc21f60beb0ad669dcfdb7c42f44ca634
CoolCloud <wxy860911@gmail.com>
2016-07-15 18:32:13 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,10 @@
 <?php
 echo 'Welcome the world of Docker !';
-var_dump($_POST); 
-echo file_get_contents('php://input');
-var_dump($_SERVER);
+$str = 'post' + var_export($_POST, true); 
+$str += 'file_get_contents' +  file_get_contents('php://input');
+$str += var_export($_SERVER, true);
+
+file_put_contents('/data/1.txt', $str);
+
+echo file_get_contents('/data/1.txt');
 ?>
